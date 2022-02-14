8727d6547f88a3688989de4b452c39872490418b
Yasuhiro <yasuhiro1026a@hotmail.com>
2022-02-15 01:00:45 +0900

    update base64 string

--- src/app/Http/Resources/Cat/CatResource.php
+++ src/app/Http/Resources/Cat/CatResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources\Cat;
 
+use App\Services\S3Service;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class CatResource extends JsonResource
@@ -17,15 +18,32 @@ class CatResource extends JsonResource
      *      @OA\Property(format="string", title="name", description="name", property="name", example="cat name"),
      *      @OA\Property(format="string", title="birth", description="birthday string", property="birth", example="2022-01-23"),
      *      @OA\Property(format="string", title="description", description="description", property="description", example="something you want to take a memo")
+     *      @OA\Property(format="string", title="picture", description="picture which is encoded with base64 format", property="picture", example="hogehoge")
      * )
      */
     public function toArray($request)
     {
+        $picture = '';
+
+        if ($this->picture !== "") {
+            $s3service = new S3Service();
+            $picture = $s3service->GetAFile($this->picture);
+
+            // get extension
+            $extension = pathinfo($this->picture, PATHINFO_EXTENSION);
+            if ($picture)
+            {
+                $picture = base64_encode($picture);
+                $picture = "data:image/{$extension};base64," . $picture;
+            }
+        }
+
         return [
             'id' => $this->id,
             'name' => $this->name,
             'birth' => $this->birth,
             'description' => $this->description,
+            'picture' => $picture,
         ];
     }
 }
