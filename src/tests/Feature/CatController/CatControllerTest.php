cb2a07eb72d6389a14fc772e7857d163fd081ed2
Yasuhiro <yasuhiro1026a@hotmail.com>
2022-07-01 00:25:36 +0900

    update cat test

--- src/tests/Feature/CatController/CatControllerTest.php
+++ src/tests/Feature/CatController/CatControllerTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature\CatController;
 
+use App\Models\Cat;
 use App\Models\User;
 use Carbon\Carbon;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
@@ -31,7 +32,7 @@ class CatControllerTest extends TestCase
      *
      * @return void
      */
-    public function test_Catのcreate_without_picture()
+    public function test_Catのcreate_without_picture_正常系()
     {
         $now = Carbon::now();
 
@@ -65,5 +66,69 @@ class CatControllerTest extends TestCase
             'user_id' => $user->id,
             'picture' => '',
         ]);
+    }
+
+    /**
+     * @test
+     *
+     * @return void
+     */
+    public function test_Catのindex_list取得_正常系()
+    {
+        $user = User::first();
+        Sanctum::actingAs($user);
+        $cats = Cat::factory()->count(3)->create([
+            'user_id' => $user->id,
+        ]);
+
+        $response = $this->get('/api/cats');
+
+        $response
+            ->assertStatus(200)
+            ->assertJson([
+                'data' => $cats->map(function($cat) {
+                    return [
+                        'id' => $cat->id,
+                        'name' => $cat->name,
+                        'birth' => (new Carbon($cat->birth))->toIso8601String(),
+                        'description' => $cat->description,
+                        'picture' => '',
+                    ];
+                })->toArray(),
+            ]);
+    }
+
+    /**
+     * @test
+     *
+     * @return void
+     */
+    public function test_Catのshow_index指定_正常系()
+    {
+        $user = User::first();
+        $anotherUser = User::factory()->create();
+
+        Sanctum::actingAs($user);
+        $cats = Cat::factory()->count(3)->create([
+            'user_id' => $user->id,
+        ]);
+
+        Cat::factory()->count(3)->create([
+            'user_id' => $anotherUser->id,
+        ]);
+
+        $response = $this->get("/api/cats/{$cats[0]->id}");
+
+        $response
+            ->assertStatus(200)
+            ->assertJson([
+                'data' => [
+                    'id' => $cats[0]->id,
+                    'name' => $cats[0]->name,
+                    'birth' => (new Carbon($cats[0]->birth))->toIso8601String(),
+                    'description' => $cats[0]->description,
+                    'picture' => '',
+                ],
+            ]);
     }
 }
